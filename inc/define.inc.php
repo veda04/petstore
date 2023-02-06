27d7f8a6dd5314d5f7d6645ac2132bc4edbb02ef
Veda Salkar <vedasalkar04@gmail.com>
2023-02-06 15:29:38 +0000

    define file changes

--- inc/define.inc.php
+++ inc/define.inc.php
@@ -2,6 +2,10 @@
 // session defines
 define("AD_SESSION_ID", "ECOM_AD"); # admin session management
 define("CU_SESSION_ID", "ECOM_CU"); # customer session management
+define("SQL_ERROR", "1");
+define("NOW", date("Y-m-d H:i:s"));
+define("NEWLINE", "\n\r");
+define("TAB_SPACE", "\t");
 
 // path defines
 define("LOG_PATH", DOCROOT."logs/"); # path to store logs
